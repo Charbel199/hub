d2357e8b965758722f2d6dc0394c3653f6230cfc
Charbel199 <charbel-boumaroun@outlook.com>
2024-10-05 15:23:12 +0300

    Remove performance monitor

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Suspense, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { PerformanceMonitor, Stats } from '@react-three/drei';
+import { Stats } from '@react-three/drei';
 import { EffectComposer, Bloom } from '@react-three/postprocessing';
 import Model from './components/model';
 import CameraController from './controller/camera_position_controller';
@@ -9,7 +9,6 @@ import Box from './components/Box'; // Assuming you have a Box component
 import './App.css';
 
 function App() {
-  const [dpr, setDpr] = useState(1);
 
   // Define your boxes here
   const boxes = [
@@ -21,25 +20,13 @@ function App() {
   return (
     <Suspense fallback={<span>loading...</span>}>
       <Canvas
-        dpr={dpr}
+        dpr={1}
         shadows
         resize={{ scroll: false }}
         style={{ width: '100vw', height: '100vh', backgroundColor: 'black' }}
         camera={{ position: [0, 5, 10] }}
       >
-        <PerformanceMonitor
-          factor={1}
-          onChange={({ factor, fps }) => {
-            console.log(`Performance Factor: ${factor}`);
-            console.log(`Frames per Second (FPS): ${fps}`);
-            setDpr(Math.floor(0.5 + 1.5 * factor));
-          }}
-          flipflops={3}
-          onFallback={() => {
-            console.log('Performance fell back to lowest possible dpr (1)');
-            setDpr(1);
-          }}
-        />
+
 
         {/* Pass boxes to CameraController */}
         <CameraController boxes={boxes} />
